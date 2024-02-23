b867f3c95cf1ed17ef927e4bddab6c754a43ba8e
Puranjay Bhargava <90250628+puranjayb@users.noreply.github.com>
2024-02-24 00:53:00 +0530

    Update DatabaseConfig.js

--- server/database/DatabaseConfig.js
+++ server/database/DatabaseConfig.js
@@ -3,9 +3,9 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
-    user: 'root',
-    password: '1234',
-    database: 'assessment'
+    user: 'your_username',
+    password: 'your_password',
+    database: 'existing_db_name'
 });
 
 connection.connect((err) => { 
